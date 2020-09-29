9b6965e52452477ead845d8ddc93ecbe6dd8a1ec
Lissettdm <lissettdm@outlook.es>
2020-09-29 17:42:30 -0300

    Update card 2

--- app-container/src/App.js
+++ app-container/src/App.js
@@ -12,28 +12,8 @@ const URL2 = "http://localhost:5005/static/index.js";
 
 const App = () => {
   const Card = useDinamycImport(URL, false);
-  // const Card2 = useDinamycImport(URL2, false);
+  const Card2 = useDinamycImport(URL2, false);
 
-  useEffect(()=> {
-    import(/* webpackIgnore: true*/ URL2)
-    .then((_exports) => {
-      console.log(_exports);
-      setTimeout(() => {
-        if(document.querySelector('tag-card')) {
-          window.customElements.define("tag-card", _exports.default)
-        }
-        ReactDOM.render(<tag-card></tag-card> ,document.querySelector("#remote-card"))
-        document.addEventListener('onClick', ()=> {
-          alert('hi')
-;        });
-
-
-      }, 2000);
-    })
-    .catch((err) => {
-      console.log(err)
-    });
-  });
 
 
   return (
@@ -49,6 +29,9 @@ const App = () => {
             </div>
             <ShadowContainer.section className="App__card-2">
               <Card text="Description" onClick={()=> alert('')}/>
+            </ShadowContainer.section>
+            <ShadowContainer.section className="App__card-2">
+              <Card2 text="Description card 2" onClick={()=> alert('')}/>
             </ShadowContainer.section>
 
           </div>
